e714560470cea9e3202758a5205244ca1700e50f
Minh Trung Nguyen <trungbb7@gmail.com>
2025-12-15 17:12:02 +0700

    feat: add sort selection feature on product page

--- src/components/pages/Products/Products.tsx
+++ src/components/pages/Products/Products.tsx
@@ -9,6 +9,8 @@ import SidebarSelector from "./SidebarSelector/SidebarSelector";
 import ProductItem from "@/components/ProductItem/ProductItem";
 import productImg1 from "@/assets/product_img1.jpg";
 import { Pagination } from "@/components/Pagination";
+import { faAngleDown, faAngleUp } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 const breadcrumbData: BreadcrumbData[] = [
   { name: "Trang chủ", path: "/", icon: <FontAwesomeIcon icon={faHouse} /> },
@@ -18,7 +20,22 @@ const breadcrumbData: BreadcrumbData[] = [
   },
 ];
 
+type SortType = "none" | "priceAsc" | "priceDesc";
+
+const sortTypeMap = {
+  none: "Sắp xếp",
+  priceAsc: "Giá tăng dần",
+  priceDesc: "Giá giảm dần",
+};
+
 export default function Products() {
+  const [selectingSort, setSelectingSort] = useState<boolean>(false);
+  const [sortType, setSortType] = useState<SortType>("none");
+
+  function toggleSelectingSort() {
+    setSelectingSort(!selectingSort);
+  }
+
   return (
     <div className="text-text1">
       <Header />
@@ -38,7 +55,46 @@ export default function Products() {
             <SidebarSelector />
           </div>
           {/* Content */}
-          <div className="w-3/4 flex flex-col px-8 pb-4">
+          <div className="w-3/4 flex flex-col gap-4 px-8 pb-4">
+            {/* head */}
+            <div className="flex justify-end">
+              <div className="relative">
+                <div
+                  onClick={toggleSelectingSort}
+                  className="flex justify-between items-center px-3 py-2 border border-gray-300 w-60 select-none cursor-pointer hover:bg-neutral-50"
+                >
+                  <span className="font-medium">{sortTypeMap[sortType]}</span>
+                  {selectingSort ? (
+                    <FontAwesomeIcon icon={faAngleUp} />
+                  ) : (
+                    <FontAwesomeIcon icon={faAngleDown} />
+                  )}
+                </div>
+                <ul
+                  className={`absolute z-100 w-full bg-white shadow ${!selectingSort && "hidden"}`}
+                >
+                  <li
+                    onClick={() => {
+                      setSortType("priceAsc");
+                      setSelectingSort(false);
+                    }}
+                    className="border border-gray-200 px-2 p-1 hover:bg-neutral-200 cursor-pointer"
+                  >
+                    Giá tăng dần
+                  </li>
+                  <li
+                    onClick={() => {
+                      setSortType("priceDesc");
+                      setSelectingSort(false);
+                    }}
+                    className="border border-gray-200 px-2 p-1 hover:bg-neutral-200 cursor-pointer"
+                  >
+                    Giá giảm dần
+                  </li>
+                </ul>
+              </div>
+            </div>
+
             <div className="flex flex-wrap gap-4 mb-8">
               <ProductItem
                 img={productImg1}
