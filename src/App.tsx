5efada8178034db5cc00b7d9428fa8bb5d6b9be5
TinyFox <galaxyeyekaizer@gmail.com>
2025-12-20 15:20:20 +0700

    feat(routing): Add routing for cart, profile, order, checkout pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,22 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/pages/Home/Home";
 import Products from "./components/pages/Products/Products";
 import NotFound from "./components/pages/NotFound/NotFound";
+import Cart from "@/components/pages/Cart/Cart.tsx";
+import Profile from "@/components/pages/Profile/InnerPages/Profile/Profile.tsx";
+import Order from "@/components/pages/Order/Order.tsx";
+import Checkout from "@/components/pages/Checkout/Checkout.tsx";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="/" element={<Home />}/>
+        <Route path="/products" element={<Products />}/>
+        <Route path="*" element={<NotFound />}/>
+        <Route path="/card" element={<Cart />}/>
+        <Route path="/profile" element={<Profile />}/>
+        <Route path="/orders" element={<Order />}/>
+        <Route path="/checkout" element={<Checkout />}/>
       </Routes>
     </BrowserRouter>
   );
