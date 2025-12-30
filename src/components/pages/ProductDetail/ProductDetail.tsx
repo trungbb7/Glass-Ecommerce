e45607ddeb04fc41367832e9dec9afaaf5265ef7
Minh Trung Nguyen <trungbb7@gmail.com>
2025-12-30 19:31:33 +0700

    setup product comparison component

--- src/components/pages/ProductDetail/ProductDetail.tsx
+++ src/components/pages/ProductDetail/ProductDetail.tsx
@@ -379,6 +379,89 @@ export default function ProductDetail() {
         <RelatedProducts products={relatedProducts} />
       </div>
       <Footer />
+
+      {/* Product Comparison */}
+      <div className="fixed w-full top-22 left-0 right-0 max-h-150 overflow-y-auto shadow z-9999">
+        <div className="mx-auto max-w-250 bg-white py-4 rounded-2xl">
+          {/* Top */}
+          <div>
+            {/* Images */}
+            <div className="flex items-center justify-around mb-6">
+              <img
+                className="size-50"
+                src="https://i.ibb.co/Y4MCy3zn/9078-c4.jpg"
+                alt="product image1"
+              />
+              <img
+                className="size-50"
+                src="https://i.ibb.co/jPvKRLF3/9078-c1.jpg"
+                alt="product image2"
+              />
+            </div>
+            {/* Titile */}
+            <div className="flex items-center mb-4">
+              <p className="flex-4 text-xl font-medium text-center">
+                KÍNH RÂM EYEPLUS 9078 C4
+              </p>
+              <p className="flex-1 text-center font-medium text-3xl">VS</p>
+              <p className="flex-4 text-xl font-medium text-center">
+                KÍNH RÂM EYEPLUS 9078 C1 GỌNG ĐEN MẮT ĐEN
+              </p>
+            </div>
+
+            <div className="w-full h-1 bg-slate-700"></div>
+
+            {/* Specification */}
+            <ul className="flex flex-col gap-1">
+              <li className="flex items-center text-center font-medium odd:bg-red-50 even:bg-white">
+                <span className="flex-4  p-4">53mm</span>
+                <span className="flex-2 p-4 bg-slate-700 text-white h-full">
+                  Độ rộng tròng
+                </span>
+                <span className="flex-4 p-4">52mm</span>
+              </li>
+              <li className="flex items-center text-center font-medium odd:bg-red-50 even:bg-white">
+                <span className="flex-4  p-4">53mm</span>
+                <span className="flex-2 p-4 bg-slate-700 text-white h-full">
+                  Độ rộng tròng
+                </span>
+                <span className="flex-4 p-4">52mm</span>
+              </li>
+              <li className="flex items-center text-center font-medium odd:bg-red-50 even:bg-white">
+                <span className="flex-4  p-4">53mm</span>
+                <span className="flex-2 p-4 bg-slate-700 text-white h-full">
+                  Độ rộng tròng
+                </span>
+                <span className="flex-4 p-4">52mm</span>
+              </li>
+              <li className="flex items-center text-center font-medium">
+                <span className="flex-4  p-4">53mm</span>
+                <span className="flex-2 p-4 bg-slate-700 text-white h-full">
+                  Độ rộng tròng
+                </span>
+                <span className="flex-4 p-4">52mm</span>
+              </li>
+
+              <li className="flex items-center text-center font-medium">
+                <span className="flex-4  p-4">53mm</span>
+                <span className="flex-2 p-4 bg-slate-700 text-white h-full">
+                  Độ rộng tròng
+                </span>
+                <span className="flex-4 p-4">52mm</span>
+              </li>
+
+              <li className="flex items-center text-center font-medium">
+                <span className="flex-4  p-4">53mm</span>
+                <span className="flex-2 p-4 bg-slate-700 text-white h-full">
+                  Độ rộng tròng
+                </span>
+                <span className="flex-4 p-4">52mm</span>
+              </li>
+            </ul>
+            <div className="w-full h-1 bg-slate-700"></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
