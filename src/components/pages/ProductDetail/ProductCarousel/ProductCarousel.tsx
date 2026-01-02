a5f02fdcd3783dce7a6fdd7bb47a0651584faef4
Minh Trung Nguyen <trungbb7@gmail.com>
2026-01-02 20:08:48 +0700

    feat: add image magnifier feature in product detail page

--- src/components/pages/ProductDetail/ProductCarousel/ProductCarousel.tsx
+++ src/components/pages/ProductDetail/ProductCarousel/ProductCarousel.tsx
@@ -3,7 +3,7 @@ import {
   faCircleChevronRight,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { forwardRef, useRef, useState } from "react";
+import { forwardRef, useRef, useState, type MouseEvent } from "react";
 
 interface ProductCarouselProp {
   images: string[];
@@ -15,6 +15,31 @@ const ProductCarousel = forwardRef<
 >(({ images }, ref) => {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
   const sliderRef = useRef<HTMLUListElement | null>(null);
+
+  const imageRef = useRef<HTMLDivElement | null>(null);
+
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [showMagnifier, setShowMagnifier] = useState(false);
+  const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
+
+  function handleMoveMove(e: MouseEvent) {
+    if (imageRef.current) {
+      const { left, top, width, height } =
+        imageRef.current.getBoundingClientRect();
+
+      const x = ((e.pageX - left - window.scrollX) / width) * 100;
+      const y = ((e.pageY - top - window.scrollY) / height) * 100;
+
+      const boundedX = Math.max(0, Math.min(x, 100));
+      const boundedY = Math.max(0, Math.min(y, 100));
+
+      setPosition({ x: boundedX, y: boundedY });
+      setCursorPosition({
+        x: e.pageX - left - window.scrollX,
+        y: e.pageY - top - window.scrollY,
+      });
+    }
+  }
 
   function slideRight() {
     if (sliderRef.current) {
@@ -37,14 +62,46 @@ const ProductCarousel = forwardRef<
   return (
     <div className="flex flex-col gap-4 grow-5 w-1/2">
       {/* Image representation */}
-      <div>
-        <img
-          ref={ref}
-          src={images[currentImageIndex]}
-          alt="img"
-          className="w-150 h-107 object-cover border border-gray-100 cursor-pointer hover:shadow-sm"
-        />
+      <div className="relative size-fit">
+        <div
+          ref={imageRef}
+          className="relative overflow-hidden cursor-crosshair"
+          onMouseEnter={() => setShowMagnifier(true)}
+          onMouseLeave={() => setShowMagnifier(false)}
+          onMouseMove={handleMoveMove}
+        >
+          <img
+            ref={ref}
+            src={images[currentImageIndex]}
+            alt="img"
+            className="w-150 h-107 object-cover border border-gray-100 cursor-pointer hover:shadow-sm"
+          />
+
+          {showMagnifier && (
+            <div
+              style={{
+                left: `${cursorPosition.x - 75}px`,
+                top: `${cursorPosition.y - 75}px`,
+              }}
+              className="absolute left-0 bottom-0 shadow bg-white/80 size-[150px]"
+            ></div>
+          )}
+        </div>
+        {showMagnifier && (
+          <div className="absolute -right-110 top-0 size-107 bg-white/80 shadow z-99">
+            <div
+              className="size-full"
+              style={{
+                backgroundImage: `url(${images[currentImageIndex]})`,
+                backgroundPosition: `${position.x}% ${position.y}%`,
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "250%",
+              }}
+            ></div>
+          </div>
+        )}
       </div>
+
       {/* Image list */}
       <div className="relative max-w-150">
         <div
