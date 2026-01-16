c3776ea179e91c3cea1ef40eba078aabcb5e0ded
TinyFox <galaxyeyekaizer@gmail.com>
2026-01-16 17:33:18 +0700

    setup(dependencies): Setup Shadcn library

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
@@ -8,7 +9,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": resolve("src"),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
   server: {
